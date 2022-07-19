720eac8957a6518bfea4214e4710fe6073b1d69c
Armando Leon <armando@pop-os.localdomain>
2022-07-19 15:35:30 -0700

    Completed server implementation

--- pkg/service/pokemon.go
+++ pkg/service/pokemon.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	pb "grpcDemo/pkg/pokemon"
 )
 
@@ -10,19 +11,54 @@ var _ pb.PokemonServiceServer = (*Pokemon)(nil)
 // Data represents the stats and name of our Pokemon
 type Data struct {
 	Name      string
-	Attack    int
-	Defense   int
-	SpDefense int
-	SpAttack  int
-	Speed     int
+	Attack    int32
+	Hp        int32
+	Defense   int32
+	SpDefense int32
+	SpAttack  int32
+	Speed     int32
 }
 
 // Pokemon is our service
 type Pokemon struct {
 	pb.UnimplementedPokemonServiceServer
+	PokemonDb map[string]*Data
+}
+
+var pokemon = make([]*Data, 0)
+
+// New creates a new Pokemon gRPC service
+func New() *Pokemon {
+	return &Pokemon{}
 }
 
 // PokeDex returns the a pokemon
 func (pdx *Pokemon) PokeDex(ctx context.Context, request *pb.PokemonRequest) (*pb.PokemonResponse, error) {
-	return nil, nil
+	if v, ok := pdx.PokemonDb[request.Name]; ok {
+		return &pb.PokemonResponse{
+			Name:      v.Name,
+			Hp:        v.Hp,
+			Atttak:    v.Attack,
+			SpAttack:  v.SpAttack,
+			SpDefense: v.SpDefense,
+			Speed:     v.Speed,
+		}, nil
+	}
+	return nil, fmt.Errorf("pokemon %s not found", request.Name)
+}
+
+func init() {
+	pokemon = append(
+		pokemon,
+		[]*Data{
+			{
+				Name:      "Pikachu",
+				Attack:    int32(55),
+				Defense:   int32(45),
+				SpAttack:  int32(55),
+				SpDefense: int32(40),
+				Speed:     int32(65),
+			},
+		}...,
+	)
 }
